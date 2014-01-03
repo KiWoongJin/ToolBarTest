6d1c53cc50e20a0b08541cf193c97f3b10cac461
KiWoongJin <jkw87first@naver.com>
2014-01-03 14:45:15 +0900

    2.9.1 툴바 실

--- ToolBarTest/ViewController.m
+++ ToolBarTest/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)handleBarButtonClick:(id)sender;
 
 @end
 
@@ -18,6 +19,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIToolbar *toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 160, 320, 60)];
+    
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc]initWithTitle:@"Button" style:UIBarButtonItemStyleDone target:self action:@selector(handleButton:)];
+    
+    NSArray *segments = [NSArray arrayWithObjects:@"A",@"B",@"C", nil];
+    UISegmentedControl *segment = [[UISegmentedControl alloc]initWithItems:segments];
+    segment.segmentedControlStyle = UISegmentedControlStyleBar;
+    segment.frame = CGRectMake(0, 0, 140, 44);
+    UIBarButtonItem *segmentButton = [[UIBarButtonItem alloc]initWithCustomView:segment];
+    
+    UIBarButtonItem *space = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(handleRefresh:)];
+    
+    NSArray *items = [NSArray arrayWithObjects:titleButton, segmentButton, space, refreshButton, nil];
+    [toolbar setItems:items];
+    
+    [self.view addSubview:toolbar];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +44,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)handleBarButtonClick:(id)sender {
+    UIBarButtonItem *button = (UIBarButtonItem *)sender;
+    if(1 == button.tag){
+        NSLog(@"왼쪽버튼");
+    }
+    else{
+        NSLog(@"오른쪽버튼");
+    }
+}
+
+- (void)handleRefresh:(id)sender{
+    
+    NSLog(@"Refresh");
+    
+}
+
+- (void)handleButton:(id)sender{
+    NSLog(@"Button Clicked");
+}
+
 
 @end
